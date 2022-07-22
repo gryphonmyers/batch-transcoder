6bb20329f1894d9ce6419abec93ed05d8898156b
Gryphon Myers <gryphonmyers@gmail.com>
2022-07-22 12:20:45 +0800

    feat: full transcode option

--- batch-transcode.js
+++ batch-transcode.js
@@ -5,7 +5,7 @@ import { createReadStream, createWriteStream, constants } from 'fs';
 import PQueue from 'p-queue';
 import Transcoder from "stream-transcoder";
 
-async function transcodeMkvFile(pathArr, outputRootDir) {
+async function transcodeMkvFile(pathArr, outputRootDir, fullTranscode) {
     const inputFilePath = path.join(...pathArr);
     const inputFileOutputPath = path.join(...[outputRootDir, ...pathArr.slice(1)]);
     const parsed = path.parse(inputFileOutputPath);
@@ -43,20 +43,21 @@ async function transcodeMkvFile(pathArr, outputRootDir) {
         await mkdir(outputDir, { recursive: true });
     }
 
-    console.log('Transcoding mkv file', inputFilePath, 'to', outputFilePath);
+    console.log(`${fullTranscode ? 'Transcoding' : 'Re-muxing'} mkv file`, inputFilePath, 'to', outputFilePath);
     
     await new Promise((resolve, reject) => {
         const inputStream = createReadStream(inputFilePath);
         const outputStream = createWriteStream(outputFilePath);
 
         new Transcoder(inputStream)
-            .videoCodec('copy')
+            .videoCodec(fullTranscode ? 'h264' : 'copy')
             .format('mp4')
+            // .custom('-movflags','+faststart')
             .on('finish', function() {    
                 resolve()
             })
             .on('error', error => {
-                reject(new Error(`Error while transcoding: ${error}`))
+                reject(new Error(`Error while processing video file ${inputFilePath}: ${error}`))
             })
             .stream().pipe(outputStream);
     })
@@ -64,7 +65,7 @@ async function transcodeMkvFile(pathArr, outputRootDir) {
     return { inputFilePath, outputFilePath, didTranscode: true }
 }
 
-async function getMkvTranscodeCbsInDir(inputDir, outputDir, transcodedFiles={}) {
+async function getMkvTranscodeCbsInDir(inputDir, outputDir, fullTranscode=false, transcodedFiles={}) {
     const inputArr = Array.isArray(inputDir) ? inputDir : [inputDir];
     const inputPath = path.join(...inputArr)
 
@@ -78,13 +79,13 @@ async function getMkvTranscodeCbsInDir(inputDir, outputDir, transcodedFiles={}) 
 
         if (stats.isDirectory()) {
             
-            const { transcodeJobs: currTranscodeJobs } = await getMkvTranscodeCbsInDir([...inputArr, file], outputDir, transcodedFiles)
+            const { transcodeJobs: currTranscodeJobs } = await getMkvTranscodeCbsInDir([...inputArr, file], outputDir, fullTranscode, transcodedFiles)
 
             transcodeJobs = transcodeJobs.concat(currTranscodeJobs)
 
         } else if (path.extname(file) == '.mkv') {
             transcodeJobs.push(async () => {
-                const { inputFilePath, outputFilePath, didTranscode } = await transcodeMkvFile([...inputArr, file], outputDir)
+                const { inputFilePath, outputFilePath, didTranscode } = await transcodeMkvFile([...inputArr, file], outputDir, fullTranscode)
                 
                 if (didTranscode) {
                     Object.assign(transcodedFiles, { [inputFilePath]: outputFilePath });
@@ -96,11 +97,11 @@ async function getMkvTranscodeCbsInDir(inputDir, outputDir, transcodedFiles={}) 
     return { transcodedFiles, transcodeJobs }
 }
 
-export async function batchTranscode(inputDir, outputDir, concurrency=os.cpus().length) {
+export async function batchTranscode(inputDir, outputDir, fullTranscode=false, concurrency=os.cpus().length) {
 
-    const { transcodedFiles, transcodeJobs } = await getMkvTranscodeCbsInDir(inputDir, outputDir);
+    const { transcodedFiles, transcodeJobs } = await getMkvTranscodeCbsInDir(inputDir, outputDir, fullTranscode);
 
-    console.log('Queueing', transcodeJobs.length, 'transcode jobs with concurrency of', concurrency);
+    console.log('Queueing', transcodeJobs.length, `${fullTranscode ? 'transcode' : 're-mux'} jobs with concurrency of`, concurrency);
 
     const queue = new PQueue({ concurrency });
 
--- cli.js
+++ cli.js
@@ -3,9 +3,24 @@ import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 import { batchTranscode } from './batch-transcode.js';
 
-const argv = yargs(hideBin(process.argv)).argv
+const argv = yargs(hideBin(process.argv))
+ .option('concurrency', {
+    alias: 'c',
+    type: 'number',
+    description: 'Number of concurrent jobs'
+  })
+  .option('ffmpeg', {
+    alias: 'f',
+    type: 'string',
+    description: 'Path to ffmpeg binary'
+  })
+  .option('transcode', {
+    alias: 't',
+    type: 'boolean',
+    description: 'Whether video stream should be transcoded'
+  }).argv
 
-const { _:[inputDir, outputDir, concurrency, ffmpegPath] } = argv;
+const { _:[inputDir, outputDir], transcode: fullTranscode, concurrency, ffmpeg: ffmpegPath } = argv;
 
 (async function(){
     if (!outputDir) throw new Error('No output directory specified')
@@ -14,7 +29,7 @@ const { _:[inputDir, outputDir, concurrency, ffmpegPath] } = argv;
     }
     console.log('Scanning for mkv files in', inputDir, 'outputting to', outputDir);
 
-    const { transcodedFiles } = await batchTranscode(inputDir, outputDir, concurrency);
+    const { transcodedFiles } = await batchTranscode(inputDir, outputDir, fullTranscode, concurrency);
 
-    console.log('Done transcoding', Object.keys(transcodedFiles).length, 'files');
+    console.log(`Done ${fullTranscode ? 'transcoding' : 're-muxing'}`, Object.keys(transcodedFiles).length, 'files');
 })();
